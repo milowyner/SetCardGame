f3750498ba6f2affec78ea7fdd8e7580bfa5bd11
Milo Wyner <milowyner@gmail.com>
2019-01-30 12:26:52 -0800

    Add ability to select and deselect cards, along with changing colors depending on how many were selected (to test coloring)

--- Set/Controller/ViewController.swift
+++ Set/Controller/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // List of UIButtons representing tappable cards
+    // List of card buttons in UI
     @IBOutlet var cardButtons: [UIButton]!
     
     @IBOutlet weak var dealMoreCardsButton: UIButton!
     @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
+    
+    // List of selected card buttons
+    var selectedCards = [UIButton]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +41,33 @@ class ViewController: UIViewController {
 
     @IBAction func cardPressed(_ sender: UIButton) {
         // Call some sort of select card method in the SetGame struct
+        // game.selectCard(sender)
+        
+        if let foundCardIndex = selectedCards.firstIndex(of: sender) {
+            selectedCards.remove(at: foundCardIndex)
+        } else {
+            selectedCards.append(sender)
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        var color: CGColor
+        if selectedCards.count < 3 {
+            color = #colorLiteral(red: 0, green: 0.4392156863, blue: 0.9607843137, alpha: 1)
+        } else if selectedCards.count > 3 {
+            color = #colorLiteral(red: 0.9607843137, green: 0.1921568627, blue: 0.1490196078, alpha: 1)
+        } else {
+            color = #colorLiteral(red: 0.1176470588, green: 0.7647058824, blue: 0.2156862745, alpha: 1)
+        }
+        for card in cardButtons {
+            card.layer.borderWidth = 0.0
+        }
+        for selectedCard in selectedCards {
+            selectedCard.layer.borderWidth = 4.0
+            selectedCard.layer.borderColor = color
+        }
     }
 }
 
