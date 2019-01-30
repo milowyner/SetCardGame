a9fa5c1d8b02568b11d7c83c6de2aae4461d4c37
Milo Wyner <milowyner@gmail.com>
2019-01-30 11:41:05 -0800

    Add comments for readablity

--- Set/Controller/ViewController.swift
+++ Set/Controller/ViewController.swift
@@ -10,20 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // List of UIButtons representing tappable cards
     @IBOutlet var cardButtons: [UIButton]!
+    
     @IBOutlet weak var dealMoreCardsButton: UIButton!
     @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set visuals of every card button
         for button in cardButtons {
             setVisuals(of: button)
         }
+        // Set visuals for other buttons in UI
         setVisuals(of: dealMoreCardsButton)
         setVisuals(of: newGameButton)
     }
     
+    // Sets visuals (corner radius and shadow) of a button.
     func setVisuals(of button: UIButton) {
         button.layer.cornerRadius = 8.0
         button.layer.shadowOpacity = 0.2
@@ -31,5 +36,8 @@ class ViewController: UIViewController {
         button.layer.shadowOffset = CGSize(width: 0.0, height: 6.0)
     }
 
+    @IBAction func cardPressed(_ sender: UIButton) {
+        // Call some sort of select card method in the SetGame struct
+    }
 }
 
