773bbd59edbf4823ae121cbddb614d154dbeb462
Milo Wyner <milowyner@gmail.com>
2019-01-30 11:26:16 -0800

    Separate files into MVC folders

---
+++ Set/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Set
+//
+//  Created by Milo Wyner on 1/30/19.
+//  Copyright © 2019 Milo Wyner. All rights reserved.
+//
+
+import Foundation
---
+++ Set/Model/SetGame.swift
@@ -0,0 +1,9 @@
+//
+//  SetGame.swift
+//  Set
+//
+//  Created by Milo Wyner on 1/30/19.
+//  Copyright © 2019 Milo Wyner. All rights reserved.
+//
+
+import Foundation
