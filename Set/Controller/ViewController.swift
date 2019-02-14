14d1933b36c8510aa87b53ba2c409a6bc23b6028
Milo Wyner <milowyner@gmail.com>
2019-02-14 15:47:24 -0800

    Implement Grid struct to layout cards more flexibly (currently doesn't work)

--- Set/Controller/ViewController.swift
+++ Set/Controller/ViewController.swift
@@ -12,25 +12,98 @@ class ViewController: UIViewController {
 
     // List of card buttons in UI
     @IBOutlet var cardButtons: [UIButton]!
-    var numberOfCardButtonsInPlay: Int = 12
+    
+    //var cardViews = [SetCardView]()
+    
+    var numberOfCardButtonsInPlay: Int = 12 {
+        didSet {
+            cardGrid.cellCount = numberOfCardButtonsInPlay
+            for index in 0..<cardGrid.cellCount {
+                if let cellRect = cardGrid[index] {
+                    let cardView = SetCardView(frame: cellRect.insetBy(dx: 8.0, dy: 8.0))
+                    view.addSubview(cardView)
+                }
+            }
+            print(numberOfCardButtonsInPlay)
+            print(cardGrid.cellCount)
+        }
+    }
+    var numberOfColumns: Int = 4
     
     @IBOutlet weak var dealMoreCardsButton: UIButton!
     @IBOutlet weak var newGameButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var verticalStackView: UIStackView!
+    
+    @IBOutlet weak var cardsContainer: UIView!
     
     // Set game
     var game = SetGame()
+    
+    // Grid to layout cards
+    lazy var cardGrid = Grid(layout: .aspectRatio(5/8))
+
     
     // Properties for cards
     let shapes: [NSAttributedString] = [NSAttributedString(string: "▲"), NSAttributedString(string: "●"), NSAttributedString(string: "■")]
     let colors = [#colorLiteral(red: 0.9108959436, green: 0.2550508642, blue: 0.2757832707, alpha: 1), #colorLiteral(red: 0.2178230739, green: 0.702655077, blue: 0.3746178072, alpha: 1), #colorLiteral(red: 0.3219855036, green: 0.2412919044, blue: 0.6032297611, alpha: 1)]
     
+    func displayCards() {
+        
+        for card in cardsContainer.subviews {
+            card.removeFromSuperview()
+        }
+        
+        for (index, _) in game.cardsInPlay.enumerated() {
+            if let cellRect = cardGrid[index] {
+                let cardView = SetCardView(frame: cellRect.insetBy(dx: 8.0, dy: 8.0))
+                cardsContainer.addSubview(cardView)
+            }
+        }
+    }
+    
+    func updateCards() {
+        for (index, cardView) in cardsContainer.subviews.enumerated() {
+            cardView.frame = cardGrid[index]!
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print(cardsContainer.frame)
+        cardGrid.frame = cardsContainer.frame
+        cardGrid.cellCount = numberOfCardButtonsInPlay
+        displayCards()
+//        print(cardGrid.dimensions)
+//        for index in 0..<cardGrid.cellCount {
+//            if let cellRect = cardGrid[index] {
+//                let cardView = SetCardView(frame: cellRect.insetBy(dx: 8.0, dy: 8.0))
+//                view.addSubview(cardView)
+//            }
+//        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Set visuals of every card button
-        for button in cardButtons {
-            setVisuals(of: button)
-        }
+        
+        
+        
+//        for _ in 1...3 {
+//            let cardView = SetCardView()
+//
+//        }
+        
+//        for _ in 1...4 {
+//            let horizontalStackView = UIStackView()
+//            horizontalStackView.axis = .horizontal
+//            horizontalStackView.distribution = .fillEqually
+//            horizontalStackView.spacing = 16
+//            for _ in 1...3 {
+//                let cardView = SetCardView()
+//                cardView.isOpaque = false
+//                horizontalStackView.addArrangedSubview(cardView)
+//            }
+//            verticalStackView.addArrangedSubview(horizontalStackView)
+//        }
+        
         // Set visuals for other buttons in UI
         setVisuals(of: dealMoreCardsButton)
         setVisuals(of: newGameButton)
@@ -58,53 +131,65 @@ class ViewController: UIViewController {
     
     @IBAction func dealThreeMoreCardsPressed(_ sender: UIButton) {
         game.dealThreeMoreCards()
-        numberOfCardButtonsInPlay += 3
-        updateUI()
+        for index in 0..<3 {
+            if let cardRect = cardGrid[index] {
+                let cardView = SetCardView(frame: cardRect.insetBy(dx: 8.0, dy: 8.0))
+                cardsContainer.addSubview(cardView)
+            }
+        }
+        updateCards()
+//        game.dealThreeMoreCards()
+//        numberOfCardButtonsInPlay += 3
+//        updateUI()
     }
     
     @IBAction func newGamePressed(_ sender: UIButton) {
-        game = SetGame()
-        numberOfCardButtonsInPlay = 12
-        updateUI()
+        numberOfCardButtonsInPlay -= 1
+        displayCards()
+//        game = SetGame()
+//        numberOfCardButtonsInPlay = 12
+//        updateUI()
     }
     
     // Updates the UI to represent the card's properties and color the card borders when selected.
     func updateUI() {
         
-        // Draw all visible card buttons using the properties from the model's cards
-        for (index, cardButton) in cardButtons.enumerated() {
-            // If the cardButton is in play
-            if index < numberOfCardButtonsInPlay {
-                cardButton.isHidden = false
-                // If there is a card to show
-                if let card = game.cardsInPlay[index] {
-                    // Enable card button
-                    cardButton.isEnabled = true
-                    cardButton.backgroundColor = UIColor.white
-                    // Update cardButton's properties based on properties of card
-                    updateProperties(of: cardButton, from: card)
-                } else {
-                    // Create an empty space where the card button was by disabling it
-                    disable(cardButton)
-                }
-            } else {
-                // Disable and hide the card button from the UI
-                disable(cardButton)
-                cardButton.isHidden = true
-            }
-        }
-        // Draw borders around cards that are selected
-        updateBorders()
+//        // Draw all visible card buttons using the properties from the model's cards
+//        for (index, cardButton) in cardButtons.enumerated() {
+//            // If the cardButton is in play
+//            if index < numberOfCardButtonsInPlay {
+//                cardButton.isHidden = false
+//                // If there is a card to show
+//                if let card = game.cardsInPlay[index] {
+//                    // Enable card button
+//                    cardButton.isEnabled = true
+//                    cardButton.backgroundColor = UIColor.white
+//                    // Update cardButton's properties based on properties of card
+//                    updateProperties(of: cardButton, from: card)
+//                } else {
+//                    // Create an empty space where the card button was by disabling it
+//                    disable(cardButton)
+//                }
+//            } else {
+//                // Disable and hide the card button from the UI
+//                disable(cardButton)
+//                cardButton.isHidden = true
+//            }
+//        }
+//        // Draw borders around cards that are selected
+//        updateBorders()
+        
+        
         
         // Update number of cards left in deck
         dealMoreCardsButton.setTitle("Deck: \(game.cardsInDeck.count)", for: .normal)
         
-        // Disable Deal 3 More Cards button if no more room or deck is empty
-        if numberOfCardButtonsInPlay == cardButtons.count || game.cardsInDeck.count == 0 {
-            dealMoreCardsButton.isEnabled = false
-        } else {
-            dealMoreCardsButton.isEnabled = true
-        }
+//        // Disable Deal 3 More Cards button if no more room or deck is empty
+//        if numberOfCardButtonsInPlay == cardButtons.count || game.cardsInDeck.count == 0 {
+//            dealMoreCardsButton.isEnabled = false
+//        } else {
+//            dealMoreCardsButton.isEnabled = true
+//        }
         
         // Update score
         scoreLabel.text = "Score: \(game.score)"
---
+++ Set/View/SetCardView.swift
@@ -0,0 +1,57 @@
+//
+//  SetCardView.swift
+//  Set
+//
+//  Created by Milo Wyner on 2/11/19.
+//  Copyright © 2019 Milo Wyner. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SetCardView: UIView {
+    
+    enum Shape {
+        case oval
+        case squiggle
+        case diamond
+    }
+    
+    enum Shading {
+        case solid
+        case striped
+        case outlined
+    }
+    
+    var shape: Shape!
+    var color: UIColor!
+    var shading: Shading!
+    var number: Int!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    private func setupView() {
+        self.isOpaque = false
+        self.layer.cornerRadius = 8.0
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowRadius = 6.0
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 6.0)
+    }
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        let cardRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        UIColor.white.setFill()
+        cardRect.fill()
+    }
+
+}
